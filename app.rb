6ee24c6556655b87a8cf306306889657d201d18a
scrumbock <scrumboc@yahoo.com>
2017-06-22 17:05:53 -0400

    added pop up modal photos to gallery

--- app.rb
+++ app.rb
@@ -5,34 +5,34 @@ include SendGrid
 
 
 get '/' do
-	@message = "Hello, this is a variable created in the default route."
+	# @message = "Hello, this is a variable created in the default route."
 	erb :index
 end
 
-# get '/contact' do
-# erb :contact
-# end
-#
-# post '/contact' do
-# 	@name = params[:email]
-# 	@subject = params[:subject]
-# 	@text = params[:text]
-#
-#
-# 	from = Email.new(email: @name)
-# 	to = Email.new(email: 'scrumboc@yahoo.com')
-# 	subject = @subject
-# 	content = Content.new(type: 'text/plain', value: @text)
-# 	mail = Mail.new(from, subject, to, content)
-#
-# 	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-# 	response = sg.client.mail._('send').post(request_body: mail.to_json)
-# 	puts response.status_code
-# 	puts response.body
-# 	puts response.headers
-# 	p @name
-# 	redirect '/'
-# end
+get '/contact' do
+erb :contact
+end
+
+post '/contact' do
+	@name = params[:email]
+	@subject = params[:subject]
+	@text = params[:text]
+
+
+	from = Email.new(email: @name)
+	to = Email.new(email: 'scrumboc@yahoo.com')
+	subject = @subject
+	content = Content.new(type: 'text/plain', value: @text)
+	mail = Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
+	p @name
+	redirect '/'
+end
 
 get '/layout' do
 	erb :layout
--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,31 @@
 
 
 var modal = document.getElementsByClassName('photo-modal')[0];
-var img = document.getElementsByClassName('pic')[0];
+var img = document.getElementsByClassName('pic');
 var modalOne = document.getElementsByClassName('modal-content')[0];
 var one = document.getElementsByClassName('one')[0];
 var span = document.getElementsByClassName('close-modal')[0];
+var imgArray = ["/images/baby-elephant.JPG", "/images/beach-wheat.JPG", "/images/between-porches.JPG", "/images/blue-buoy.JPG", "/images/blue-canoe.JPG", "/images/boat-ripple.JPG", "/images/carter-sail.JPG", "/images/carter-snow.JPG", "/images/down-stairs.JPG", "/images/fence-grass.JPG", "/images/grass-boat.JPG", "/images/low-water.JPG"];
 
-img.onclick = function(){
-    modal.style.display = "block";
-    modalOne.src = one;
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalOne.src = one;
+// }
+
+
+for(let i=0;i<img.length;i++){
+  img[i].addEventListener("click", function (){openModal(i)})
 }
 
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
+function openModal(x) {
+  modal.style.display = "block";
+  modalOne.style.backgroundImage = "url('" + imgArray[x] + "')";
+
+}
+
+console.log(imgArray[0])
