570b00d392307064d676a7939e7c22890cc543c4
scrumbock <scrumboc@yahoo.com>
2017-06-23 17:25:34 -0400

    added cool scrolling feature to gallery

--- public/js/script.js
+++ public/js/script.js
@@ -7,13 +7,14 @@ var one = document.getElementsByClassName('one')[0];
 var span = document.getElementsByClassName('close-modal')[0];
 var imgArray = ["/images/baby-elephant.JPG", "/images/beach-wheat.JPG", "/images/between-porches.JPG", "/images/blue-buoy.JPG", "/images/blue-canoe.JPG", "/images/boat-ripple.JPG", "/images/carter-sail.JPG", "/images/carter-snow.JPG", "/images/down-stairs.JPG", "/images/fence-grass.JPG", "/images/grass-boat.JPG", "/images/low-water.JPG"];
 var caption = document.getElementsByClassName('caption')[0];
-
+var gallery = document.getElementsByClassName("gallery")[0];
 
 for(let i=0;i<img.length;i++){
   img[i].addEventListener("click", function (){openModal(i)})
 }
 
 span.onclick = function() {
+  console.log("span");
   modal.style.display = "none";
 }
 
---
+++ public/js/scroll.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('DOMContentLoaded', scroll);
+
+function scroll () {
+  window.scrollTo(0, 1350);
+}
